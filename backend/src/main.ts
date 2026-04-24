5733bfd8726dbc349495cc867fc3061c8e87757b
Luis Fernando <luisfffarias27@gmail.com>
2026-04-24 19:03:13 -0300

    Fix: porta atualizada

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -28,7 +28,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT || 3000);
   console.log(`🚀 MaidInBrazil rodando em: http://localhost:3000/api`);
   
 }
