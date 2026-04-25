fd26d20183f77c1006060689b3f821928989aee4
Luis Fernando <luisfffarias27@gmail.com>
2026-04-25 11:50:58 -0300

    Fix: Cors habilitado

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,19 +2,21 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import { ValidationPipe } from '@nestjs/common'; // 1. Importação necessária
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
+  // 👇 ADICIONE ESTA LINHA: Habilita o CORS para o Front-end conseguir conectar!
+  app.enableCors();
 
-  // 2. Ativação da Validação Global (Crucial para os DTOs funcionarem)
+  // Ativação da Validação Global
   app.useGlobalPipes(
     new ValidationPipe({
-      whitelist: true, // Remove campos extras que não estão no DTO
-      forbidNonWhitelisted: true, // Retorna erro se enviarem campos não permitidos
-      transform: true, // Converte tipos automaticamente (ex: string para número se necessário)
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
     }),
   );
 
@@ -29,8 +31,8 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document);
 
   await app.listen(process.env.PORT || 3000);
-  console.log(`🚀 MaidInBrazil rodando em: http://localhost:3000/api`);
-  
+  console.log(`🚀 MaidInBrazil rodando em: http://localhost:${process.env.PORT || 3000}/api`);
 }
+
 console.log('DATABASE_URL:', process.env.DATABASE_URL);
 bootstrap();
